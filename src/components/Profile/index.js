85baba1a6efe5783baf6780d11c0e869eaa4b432
arb1ona <arbiona.shahu@gmail.com>
2021-01-08 22:45:45 +0100

    CV link updated

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,61 +1,56 @@
-import React, { useContext } from 'react';
-import './index.scss'
+import React, { useContext } from "react";
+import "./index.scss";
 import TypedText from "../TypedText";
 import SocialLinks from "../SocialLinks";
 import Info from "../Info";
-import placeholder from "../../assets/placeholder.png"
+import placeholder from "../../assets/placeholder.png";
 import Skills from "../Skills";
-import {ThemeContext} from "../../context/ThemeContext";
+import { ThemeContext } from "../../context/ThemeContext";
 import { useAnimation } from "../../use/animation";
-import arbiona from '../../assets/arbiona.png'
+import arbiona from "../../assets/arbiona.png";
 
 const Profile = () => {
-    const { backgroundImage, type } = useContext(ThemeContext);
-    const { animation } = useAnimation();
-    return (
-        <div className="profile">
-            <div className="profile__banner">
-                <img src={arbiona} alt={type} />
-            </div>
-            {/* <div className="profile__pic">
+  const { backgroundImage, type } = useContext(ThemeContext);
+  const { animation } = useAnimation();
+  return (
+    <div className="profile">
+      <div className="profile__banner">
+        <img src={arbiona} alt={type} />
+      </div>
+      {/* <div className="profile__pic">
                 <img src={arbiona} alt={type} />
             </div> */}
-            <div className="profile__content">
-                <div className="profile__title">
-                    Arbiona Shahu
-                </div>
-                {/* <div className="profile__stack">
+      <div className="profile__content">
+        <div className="profile__title">Arbiona Shahu</div>
+        {/* <div className="profile__stack">
                 Full Stack Developer
                 </div> */}
-                <TypedText
-                  dataText={
-                      [
-                          'Full Stack Developer',
-                          'Architect',
-                          'Graphic Designer'
-                      ]
-                  }
-                />
-                <div className="profile__location">
-                <i class="fas fa-map-marker-alt"></i>                    {/* <img src={placeholder} style={{ width: "20px" }} /> */}
-                    Tirana, Albania
-                </div>
-                <Skills />
-            </div>
-            <div className="profile__contact">
-                <a href="https://drive.google.com/file/d/1AFJ6JtDXZAMbNPm-Tc9C0v8-dregQg4d/view?usp=sharing" target="_blank" rel="noreferrer">
-                        <span>
-                            Download CV
-                        </span>
-                </a>
-                <a href="mailto:arbiona.shahu@gmail.com">
-                        <span>
-                        <SocialLinks/>
-                        </span>
-                </a>
-            </div>
+        <TypedText
+          dataText={["Full Stack Developer", "Architect", "Graphic Designer"]}
+        />
+        <div className="profile__location">
+          <i class="fas fa-map-marker-alt"></i>{" "}
+          {/* <img src={placeholder} style={{ width: "20px" }} /> */}
+          Tirana, Albania
         </div>
-    );
+        <Skills />
+      </div>
+      <div className="profile__contact">
+        <a
+          href="https://drive.google.com/file/d/1SjPX5YCM5-gQzLzuvkBIrpdlylxNGl-A/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span>Download CV</span>
+        </a>
+        <a href="mailto:arbiona.shahu@gmail.com">
+          <span>
+            <SocialLinks />
+          </span>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
