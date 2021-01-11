ce4146f2c8989dff06860b5dc8fd06cd9158dca7
arb1ona <arbiona.shahu@gmail.com>
2021-01-11 10:33:26 +0100

    Covid-19 Tracker Addded

--- src/pages/Works/data.js
+++ src/pages/Works/data.js
@@ -1,15 +1,24 @@
 export const projects = [
-    
-    {
-        title: 'Appartner - Home & Flatmate Finder',
-        images: [
-            'https://user-images.githubusercontent.com/63557021/99974004-ac14e580-2da0-11eb-88f0-173f539d0f98.png',
-            'https://user-images.githubusercontent.com/63557021/99976054-4118de00-2da3-11eb-963d-4bf7ba0c2fe4.png',
-'https://user-images.githubusercontent.com/63557021/99976695-111e0a80-2da4-11eb-81a8-25a5aad1a3f5.png'
-        ],
-        sourceCodeLink: 'https://github.com/arb1ona/Appartner',
-        demoLink: 'https://appartner.herokuapp.com/',
-        type: 'react'
-    }
-    
-]
+  {
+    title: "Appartner - Home & Flatmate Finder",
+    images: [
+      "https://user-images.githubusercontent.com/63557021/99974004-ac14e580-2da0-11eb-88f0-173f539d0f98.png",
+      "https://user-images.githubusercontent.com/63557021/99976054-4118de00-2da3-11eb-963d-4bf7ba0c2fe4.png",
+      "https://user-images.githubusercontent.com/63557021/99976695-111e0a80-2da4-11eb-81a8-25a5aad1a3f5.png",
+    ],
+    sourceCodeLink: "https://github.com/arb1ona/Appartner",
+    demoLink: "https://appartner.herokuapp.com/",
+    type: "react",
+  },
+  {
+    title: "Covid-19 Tracker",
+    images: [
+      "https://user-images.githubusercontent.com/63557021/104164220-e7727e80-53f7-11eb-9f09-35e5a6af18cb.png",
+
+      "https://user-images.githubusercontent.com/63557021/104164500-4932e880-53f8-11eb-8864-c93475006be6.png",
+    ],
+    sourceCodeLink: "https://github.com/arb1ona/Appartner",
+    demoLink: "https://github.com/arb1ona/COVID-19-TRACKER",
+    type: "react",
+  },
+];
